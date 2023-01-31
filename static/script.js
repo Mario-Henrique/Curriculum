b2d30c98fe61908bcc55d699de5b8b3ec39f4fb2
Mario Henrique <dark_mh@hotmail.com>
2023-01-31 19:44:57 -0300

    Menu lateral responsivo

---
+++ static/script.js
@@ -0,0 +1,9 @@
+/* Controle do Menu Lateral */
+const menuMobile = document.querySelector('.menu-mobile')
+const body = document.querySelector('body')
+
+menuMobile.addEventListener('click', () =>{
+    menuMobile.classList.contains("bi-list") ? menuMobile.classList.replace("bi-list", "bi-x") : menuMobile.classList.replace("bi-x", "bi-list")
+
+    body.classList.toggle("menu-nav-active")
+})
