7a294eb594bfd7819e269789c2db43bcee37e88e
Mario Henrique <dark_mh@hotmail.com>
2023-02-01 20:19:48 -0300

    Section sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -7,3 +7,15 @@ menuMobile.addEventListener('click', () =>{
 
     body.classList.toggle("menu-nav-active")
 })
+
+/* Fechar o menu ao escolher um item */
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
+})
